8e9df9c426886c84c2b042f5c71e58a4aac956b7
lovegupta112 <loveguptaofficials@gmail.com>
2023-08-29 01:10:28 +0530

    updated code

--- problem6.js
+++ problem6.js
@@ -7,7 +7,7 @@
     let carsData=[];
     for(let i=0;i<inventory.length;i++){
         if(inventory[i].car_make==='BMW' || inventory[i].car_make==='Audi'){
-           carsData.push(inventory[i].car_model);
+           carsData.push(inventory[i]);
         }
     }
     return JSON.stringify(carsData);
