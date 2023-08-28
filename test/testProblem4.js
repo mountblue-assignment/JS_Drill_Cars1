07bd9e4d2912e0d07fc1dbb67c7ca54f226c1a4b
lovegupta112 <loveguptaofficials@gmail.com>
2023-08-28 15:59:03 +0530

    solved problem4.js

---
+++ test/testProblem4.js
@@ -0,0 +1,7 @@
+const {inventory}=require('../inventory');
+const showAllCarYearsFun=require('../problem4');
+
+
+const result=showAllCarYearsFun(inventory);
+
+console.log(`All Car Years: ${result}`);
