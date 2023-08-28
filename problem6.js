d2264890977e16dd4073dc17cbec85920b463def
lovegupta112 <loveguptaofficials@gmail.com>
2023-08-28 16:33:22 +0530

    solved problem6.js

---
+++ problem6.js
@@ -0,0 +1,16 @@
+  // ==== Problem #6 ====
+  // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Execute a function and return an array that only contains BMW and Audi cars.  Once you have the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+
+
+ function findBMWandAudiCars(inventory){
+ 
+    let carsData=[];
+    for(let i=0;i<inventory.length;i++){
+        if(inventory[i].car_make==='BMW' || inventory[i].car_make==='Audi'){
+           carsData.push(inventory[i].car_model);
+        }
+    }
+    return JSON.stringify(carsData);
+ }
+
+ module.exports=findBMWandAudiCars;
